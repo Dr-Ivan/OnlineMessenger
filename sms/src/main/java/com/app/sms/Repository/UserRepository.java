d342894b9399ce5c8398f308cff46eceb7f81232
IvanDrv <ivander05@mail.ru>
2026-01-05 15:48:23 +0300

    Pagination for get all user names query

--- sms/src/main/java/com/app/sms/Repository/UserRepository.java
+++ sms/src/main/java/com/app/sms/Repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.app.sms.Repository;
 
 import com.app.sms.Entity.UserEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +17,9 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
     boolean existsByNameAndPasswordHash(String name, String pass);
 
     Optional<UserEntity> findByName(String name);
+
+    @Query(value = """
+            SELECT name FROM users
+            """, nativeQuery = true)
+    List<String> getUserNamesPaged(Pageable pageable);
 }
